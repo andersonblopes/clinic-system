2f9fe662b9325e94463fe56cc1197adf7231bada
Anderson Lopes <anderson.lopes.2010@gmail.com>
2020-11-09 20:36:40 +0100

    Creates project.

---
+++ src/test/java/com/lopes/clinic/ClinicSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lopes.clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
